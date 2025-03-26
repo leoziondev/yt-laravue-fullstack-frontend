d66b2eaecf79af4e4f110ed2a44aa34c19ce9aad
leoziondev <leozionsuporte@gmail.com>
2025-03-26 20:46:17 -0300

    feat: add pages

---
+++ src/pages/MyImages.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+const images = [
+    {id: 1, label: 'Test 01', url: 'http://localhost'},
+    {id: 2, label: 'Test 02', url: 'http://localhost'},
+    {id: 3, label: 'Test 03', url: 'http://localhost'},
+]
+
+</script>
+
+<template>
+    <header class="bg-white shadow-sm">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Images</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div v-for="image in images" :key="image.id" class="bg-white overflow-hidden shadow rounded-lg">
+                <img :src="image.url" alt="Image" class="w-full h-48 object-contain">
+                <div class="px-4 py-4">
+                    <h3 class="text-lg font-semibold text-gray-900">{{ image.name }}</h3>
+                    <p class="text-sm text-gray-500 mb-4">{{ image.label }}</p>
+                    
+                </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
